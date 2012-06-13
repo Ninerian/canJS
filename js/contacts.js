4a3233529655e2b9be5efc896c8161d4f3fe3032
Daniel Große <hallo@daniel-der-grosse.de>
2012-06-13 08:25:14 +0200

    Added filtering (part2)

--- js/contacts.js
+++ js/contacts.js
@@ -54,6 +54,23 @@ Category = can.Model({
   findAll: 'GET /categories'
 },{});
 
+Contact.List = can.Model.List({
+  filter: function(category){
+    this.attr('length');
+    var contacts = new Contact.List([]);
+    this.each(function(contact, i){
+      if(category === 'all' || category === contact.attr('category')) {
+        contacts.push(contact);
+      }
+    });
+    return contacts;
+  },
+
+  count: function(category) {
+    return this.filter(category).length;
+  }
+});
+
 can.fixture('GET /contacts', function(){
   return [CONTACTS];
 });
@@ -84,15 +101,44 @@ Contacts = can.Control({
   }
 });
 
-$(document).ready(function(){
-  $.when(Category.findAll(), Contact.findAll()).then(
-    function(categoryResponse, contactResponse){
-      var categories = categoryResponse[0],
-        contacts = contactResponse[0];
+/**
+ * Kategorien anzeigen
+ */
 
-      new Contacts('#contacts', {
-        contacts: contacts,
-        categories: categories
-      });
-  });
+Filter = can.Control({
+  init: function(){
+    var category = can.route.attr('category') || "all";
+    this.element.html(can.view('views/filterView', {
+      contacts: this.options.contacts,
+      categories: this.options.categories
+    }));
+    this.element.find('[data-category="' + category + '"]').parent().addClass('active');
+  },
+  '[data-category] click': function(el, ev) {
+    this.element.find('[data-category]').parent().removeClass('active');
+    el.parent().addClass('active');
+    can.route.attr('category', el.data('category'));
+  }
 });
+
+/**
+ *  Verwaltet URL um AJAX-Änderungen anzuzeigen
+ */
+can.route( 'filter/:category' );
+can.route('', {category: 'all' });
+
+$(document).ready(function(){
+  $.when(Category.findAll(), Contact.findAll()).then(function(categoryResponse, contactResponse){
+    var categories = categoryResponse[0],
+      contacts = contactResponse[0];
+
+    new Contacts('#contacts', {
+      contacts: contacts,
+      categories: categories
+    });
+    new Filter('#filter', {
+      contacts: contacts,
+      categories: categories
+    });
+  });
+})
